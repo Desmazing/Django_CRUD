766d3f7a6dd9cde1526b790607cbfd2746adc4ed
Desmazing <dkimodera1@gmail.com>
2022-06-22 17:58:26 +0300

    django CRUD

--- I4G015317NRI/blog/urls.py
+++ I4G015317NRI/blog/urls.py
@@ -8,6 +8,5 @@ urlpatterns = [
     path("create/", views.PostCreateView.as_view(), name="post_create"),
     path("delete/<slug:slug>", views.PostDeleteView.as_view(), name="post_delete"),
     path("update/<slug:slug>", views.PostUpdateView.as_view(), name="post_update"),
-    path("read/<slug:slug>", views.PostDetailView.as_view(), name="post_detail"),
-
+    path("read/<slug:slug>", views.PostDetailView.as_view(), name="post_detail")
 ]
